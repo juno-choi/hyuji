2b047250027a47bed01de3f42c6d4c558d7a0fd6
juno-choi <ililil9482@naver.com>
2023-03-29 09:30:52 +0900

    [기능] 댓글 페이징 불러오기 문서화

--- src/main/java/com/juno/normalapi/controller/board/BoardController.java
+++ src/main/java/com/juno/normalapi/controller/board/BoardController.java
@@ -7,6 +7,7 @@ import com.juno.normalapi.domain.dto.RequestBoard;
 import com.juno.normalapi.domain.dto.RequestReply;
 import com.juno.normalapi.domain.vo.BoardListVo;
 import com.juno.normalapi.domain.vo.BoardVo;
+import com.juno.normalapi.domain.vo.ReplyListVo;
 import com.juno.normalapi.domain.vo.ReplyVo;
 import com.juno.normalapi.service.board.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @RestController
@@ -38,7 +40,7 @@ public class BoardController {
     }
 
     @GetMapping("")
-    public ResponseEntity<Response<BoardListVo>> board(@PageableDefault(size = 5) Pageable pageable, HttpServletRequest request){
+    public ResponseEntity<Response<BoardListVo>> getBoardList(@PageableDefault(size = 5) Pageable pageable, HttpServletRequest request){
         return ResponseEntity.ok(Response.<BoardListVo>builder()
                 .code(ResponseCode.SUCCESS)
                 .message("성공")
@@ -61,6 +63,21 @@ public class BoardController {
                 .code(ResponseCode.SUCCESS)
                 .message("성공")
                 .data(boardService.getBoard(boardId, request))
+                .build());
+    }
+
+    @GetMapping("/reply")
+    public ResponseEntity<Response<ReplyListVo>> getReplyList(
+            @RequestParam(name = "board_id")
+            @NotNull(message = "board_id는 필수값 입니다.")
+            @Min(value = 1, message = "board_id 최소 값은 1입니다.") Long boardId,
+            @PageableDefault(size = 5) Pageable pageable,
+            HttpServletRequest request
+    ){
+        return ResponseEntity.ok(Response.<ReplyListVo>builder()
+                .code(ResponseCode.SUCCESS)
+                .message("성공")
+                .data(boardService.getReplyList(boardId, pageable, request))
                 .build());
     }
 }
